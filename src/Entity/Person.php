1d64351d14426d9463e1f4ffa5c2976da4e8363c
SimplyyAlex <maseal@seznam.cz>
2025-02-24 03:06:52 +0100

    First changes

---
+++ src/Entity/Person.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PersonRepository")
+ * @ORM\Table(name="people")
+ */
+class Person
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $surname;
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function getName(): ?string {
+        return $this->name;
+    }
+
+    public function setName(string $name): self {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getSurname(): ?string {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): self {
+        $this->surname = $surname;
+        return $this;
+    }
+}
